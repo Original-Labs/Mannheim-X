8310cdb4f7e5f8361b44010a09035eda714294e0
JensenLyou <897481138@qq.com>
2021-12-25 22:41:01 +0800

    feat: remove muti-luanguage and chinese remak

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -10,18 +10,6 @@ const SpinContainer = styled('div')`
   }
 `
 
-/**
- *
- * @param {
- *          param {boolean} loading 是否加载
- *          param {ReactNode} indicator 加载指示符(自定义icon)
- *          param {number} delay 加载延迟多少秒
- *          param {boolean} spining 是否为加载状态
- *          param {ReactNode} tip 当作包裹元素时,可以自定义描述文案
- *          param {string} wrapperClassName 包装器的类属性
- *        } props
- * @returns
- */
 export default function Loading(props) {
   return props.loading ? (
     <SpinContainer>
