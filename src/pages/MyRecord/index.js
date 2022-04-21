539b92638191a2f0509ef4b8670e870817f5ff7c
wanbowen <wbw157@gmail.com>
2022-04-21 18:07:26 +0800

    feat: 认购记录逆序,获取本地时间

--- src/pages/MyRecord/index.js
+++ src/pages/MyRecord/index.js
@@ -86,6 +86,7 @@ export default ({ match }) => {
               exchangeContractTxData.push(txData[i])
             }
           }
+          exchangeContractTxData.reverse()
           console.log('exchangeContractTxData', exchangeContractTxData)
           // 已经存在旧数据，判定新旧数据是否一致，一致则不进行setSubscribeList
           if (
@@ -158,7 +159,7 @@ export default ({ match }) => {
     return numberStr / 10 ** 18
   }
   const getDateTime = item => {
-    return new Date(item * 1000).toUTCString().substring(5)
+    return new Date(item * 1000).toLocaleString()
   }
 
   useEffect(() => {
