c158a912aba1f93199b5301340cc5d8563d766c7
wanbowen <wbw157@gmail.com>
2022-04-22 18:09:20 +0800

    repair: 修复钱包连接时显示异常

--- src/routes/Renew.js
+++ src/routes/Renew.js
@@ -4,16 +4,25 @@ import { Redirect } from 'react-router-dom'
 import { useAccount } from '../components/QueryAccount'
 import Banner from '../components/Banner'
 import { emptyAddress } from '../utils/utils'
+import { message } from 'antd'
 
 export default function Renew(props) {
   const account = useAccount()
   if (account !== emptyAddress) {
     return <Redirect to={'/'} />
   }
+
   return (
     <Banner>
-      You are here because of a transaction you completed. Please login to your
-      wallet to be redirected
+      {// 弹窗提示
+      message.warning({
+        key: 2,
+        content: '正在连接钱包...',
+        duration: 2
+        // style: { marginTop: '20vh' }
+      })}
+      {/*You are here because of a transaction you completed. Please login to your*/}
+      {/*wallet to be redirected*/}
     </Banner>
   )
 }
